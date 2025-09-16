407b4e1867c3c97ee593549411240fbeba94311c
brencarovit-ops <brencarovit@gmail.com>
2025-09-16 19:45:17 -0300

    Add files via upload

---
+++ BRENDAA.index.erl
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <title>Soy de IES</title>
+</head>
+<body style="text-align:center; font-family: Arial, sans-serif; margin-top: 50px;">
+    <h1>Soy de IES</h1>
+    <a href="https://www.ies21.edu.ar/" target="_blank" rel="noopener noreferrer">
+        <img src="https://www.ies21.edu.ar/img/logos_nuevos/LOGO_IES.png" alt="Logo IES" style="width:200px;" />
+    </a>
+</body>
+</html>
